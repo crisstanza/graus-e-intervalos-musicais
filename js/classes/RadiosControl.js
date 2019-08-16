d9648d50ac2018697db50c2ff66aaf710fb2f416
crisstanza <crisstanza@work>
2019-08-15 21:22:12 -0300

    Início da marcação das notas conforme o tom e o modo grego.

--- js/classes/RadiosControl.js
+++ js/classes/RadiosControl.js
@@ -2,11 +2,13 @@ var RadiosControl = function() {
 };
 
 RadiosControl.prototype.roots_OnClick = function(event) {
-	let radio = event.target;
-	console.log(radio.value)
+	// let radio = event.target;
+	// console.log(radio.value);
+	updateNotas(event);
 };
 
 RadiosControl.prototype.greekModes_OnClick = function(event) {
-	let radio = event.target;
-	console.log(radio.value)
+	// let radio = event.target;
+	// console.log(radio.value);
+	updateNotas(event);
 };
--- js/js.js
+++ js/js.js
@@ -1,6 +1,41 @@
+var ROOTS_INDEX = {
+	'c': 0,
+	'd': 2,
+	'e': 4,
+	'f': 5,
+	'g': 7,
+	'a': 9,
+	'b': 12
+};
+
+var GREEK_MODES_INTERVALS = {
+	'jonico': [2, 2, 1, 2, 2, 2, 1],
+	'dorico': [2, 1, 2, 2, 2, 1, 2],
+	'frigio': [1, 2, 2, 2, 1, 2, 2],
+	'lidio': [2, 2, 2, 1, 2, 2, 1],
+	'mixolidio': [2, 2, 1, 2, 2, 1, 2],
+	'eolico': [2, 1, 2, 2, 1, 2, 2],
+	'locrio': [1, 2, 2, 1, 2, 2, 2]
+};
+
+function updateNotas(event) {
+	let currentRoot = tdRoot.querySelector('[type=radio][name=roots]:checked');
+	let currentGreekMode = tdGreekMode.querySelector('[type=radio][name=greek-modes]:checked');
+	let trs = tbNotas.querySelectorAll('tr');
+	$.forEach(trs, function(tr, indexTr) {
+		let tds = tr.querySelectorAll('td');
+		$.forEach(tds, function(td, indexTd) {
+			$.addClass(td, 'bold');
+		});
+	});
+	// console.log(currentRoot, currentGreekMode);
+}
+
 (function() {
 
-	function initNotas() {
+	let Autos = io.github.crisstanza.Autos;
+
+	function initLayoutNotas(event) {
 		let tds = tbNotas.querySelectorAll('td');
 		$.forEach(tds, function(td, index) {
 			if (td.innerHTML.includes('#')) {
@@ -11,9 +46,10 @@
 		});
 	}
 
-	function window_Load() {
-		io.github.crisstanza.Autos.initRadios(new RadiosControl());
-		initNotas();
+	function window_Load(event) {
+		Autos.initRadios(new RadiosControl());
+		initLayoutNotas(event);
+		updateNotas(event);
 	}
 
 	window.addEventListener('load', window_Load);
